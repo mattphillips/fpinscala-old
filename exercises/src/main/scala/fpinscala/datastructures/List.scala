b2a0c918ac1b8d4a41cada2b1938cf7077889e63
Matt Phillips <mattphillips1016@gmail.com>
2016-06-01 17:17:34 +0100

    completed exercise 3.6

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -75,7 +75,13 @@ object List { // `List` companion object. Contains functions for creating and wo
     }
   }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => Nil
+      case Cons(x,xs) => Cons(x,init(xs))
+    }
+  }
 
   def length[A](l: List[A]): Int = sys.error("todo")
 
